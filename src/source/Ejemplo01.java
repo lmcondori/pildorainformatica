d949ae94b0f3f80df8bd940613b0b27b5af3f396
lmcondori <lmcondori@gmail.com>
2016-01-13 09:42:17 -0500

    Ejemplo de envio a github 2

--- src/source/Ejemplo01.java
+++ src/source/Ejemplo01.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package source;
 
-/**
- *
- * @author usuariojr
- */
 public class Ejemplo01 {
     public void Ejemplo01(){
         
